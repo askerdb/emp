d3dbb5c95f13cb53ffc60c5fdd344139c6805d77
Asker Daniel Brejnrod <chs962@cbmrcomp02fl.sund.root.ku.dk>
2021-01-22 10:40:39 +0100

    test script

--- askers_stuff/nmfer.py
+++ askers_stuff/nmfer.py
@@ -2,7 +2,9 @@ import pickle as pkl
 import nimfa
 import time
 import numpy as np
-
+import umap
+import umap.plot
+import pickle as pkl
 def row_filter_intensity(X, bin_names, threshold = 1/1000):
     colsums = np.array(X.sum(axis = 0)).flatten()
     for i in range(X.shape[1]):
@@ -27,10 +29,16 @@ def row_filter_intensity_csc(X, bin_names, threshold = 1/1000):
 
 X, bins, col_names = pkl.load(open("1907_EMPv2_INN_GNPS.mgf.pkl", 'rb'))
 start = time.time()
-X_f, bins_f = row_filter_intensity_csc(X, bins, threshold = 1/100)
+X_f, bins_f = row_filter_intensity_csc(X, bins, threshold = 1/1)
 print('It took {0:0.1f} seconds to do csc filter'.format(time.time() - start))
 print(X_f.shape)
-start = time.time()
-nmf_model = nimfa.Nmf(X_f, rank=2)
-nmf_fit = nmf_model()
+#start = time.time()
+#nmf_model = nimfa.Nmf(X_f, rank=2)
+#nmf_fit = nmf_model()
 print('It took {0:0.1f} seconds to do NMF processes'.format(time.time() - start))
+
+start = time.time()
+mapper = umap.UMAP(metric='cosine', random_state=42, low_memory=True).fit(X_f)
+umap.plot.points(mapper)
+print('It took {0:0.1f} seconds to do NMF processes'.format(time.time() - start))
+pkl.dump(mapper, open("1907_EMPv2_INN_GNPS.mgf.pkl", "wb"))
